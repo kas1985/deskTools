b3abfe53701ef5ba36c02c05b581ab57afc108ce
Karlos Martinez <57291456+kas1985@users.noreply.github.com>
2026-06-04 14:33:41 +0200

    Add app package

---
+++ app/data.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+NAV_ITEMS = [
+    ("Resumen", ft.Icons.MONITOR_HEART_OUTLINED, ft.Icons.MONITOR_HEART),
+    ("Procesos", ft.Icons.LIST_ALT_OUTLINED, ft.Icons.LIST_ALT),
+    ("Modo juegos", ft.Icons.SPORTS_ESPORTS_OUTLINED, ft.Icons.SPORTS_ESPORTS),
+    ("Video", ft.Icons.DOWNLOAD_OUTLINED, ft.Icons.DOWNLOAD),
+    ("Almacenamiento", ft.Icons.STORAGE_OUTLINED, ft.Icons.STORAGE),
+    ("Red", ft.Icons.LAN_OUTLINED, ft.Icons.LAN),
+]
